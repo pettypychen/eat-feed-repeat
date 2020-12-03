108ada3ac7694b4fb78702120c8d28d65b7191b7
Petty PY Chen <39106042+pettypychen@users.noreply.github.com>
2020-12-03 18:56:09 +0800

    Add files via upload

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'Eat, Feed, & Repeat',
+  shortName: 'Eat, Feed, and Repeat',
+  description: "So basically, you are a 🙍🏻‍♀️🙍🏻‍♂️‍ who wants a #bosslife 🙌🏻 and to travel the 🌎🏝 but instead you are raising a 👶🏻. Well, we got ya! 😉 ",
+  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'P Chen',
+  twitter: 'P_Chen'
+}
