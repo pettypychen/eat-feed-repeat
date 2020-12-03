e9eab9b8e7447476d7f61f22a52a44284bdc19b1
Petty PY Chen <39106042+pettypychen@users.noreply.github.com>
2020-12-03 22:37:48 +0800

    added GA

--- siteConfig.js
+++ siteConfig.js
@@ -2,8 +2,17 @@ module.exports = {
   name: 'Eat, Feed, & Repeat',
   shortName: 'Eat, Feed, and Repeat',
   description: "So basically, you are a 🙍🏻‍♀️🙍🏻‍♂️‍ who wants a #bosslife 🙌🏻 and to travel the 🌎🏝 but instead you are raising a 👶🏻. Well, we got ya! 😉 ",
-  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  url: 'https://eatfeedrepeat.com', // Domain of your site without prefix!
   prefix: '/',
   author: 'P Chen',
-  twitter: 'P_Chen'
+  instagram: 'https://www.instagram.com/pettypychen/',
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-YNJ90NJCP9R",
+      },
+    },
+  ],
 }
